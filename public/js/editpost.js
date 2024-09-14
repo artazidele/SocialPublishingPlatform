dfbe65923d9237f067df573480a440701cb7b74d
artazidele <77989249+artazidele@users.noreply.github.com>
2024-09-14 10:05:47 +0300

    fix: remove comments from js files

--- public/js/editpost.js
+++ public/js/editpost.js
@@ -2,12 +2,6 @@ function setPreviousValues() {
     document.getElementById('titleI').value = document.getElementById('titleP').value;
     document.getElementById('contentI').value = document.getElementById('contentP').value;
 
-    // var ESAPI = require('node-esapi');
-    // const titlePValue = document.getElementById('titleP').value;
-    // const contentPValue = document.getElementById('contentP').value;
-    // document.getElementById('titleI').value = '<%=ESAPI.encoder().encodeForJavascript(titlePValue)%>';
-    // document.getElementById('contentI').value = '<%=ESAPI.encoder().encodeForJavascript(contentPValue)%>';
-
     const postCategories = JSON.parse(document.getElementById('postCategoriesP').value);
     const postCategoryCheckboxes = document.getElementsByName('categories[]');
     postCategoryCheckboxes.forEach(checkbox => {
